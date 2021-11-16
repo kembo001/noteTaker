6912601b30d9b5098f33a977d13967e6e1bd3860
kembo001 <47574348+kembo001@users.noreply.github.com>
2021-11-15 18:49:26 -0600

    Note Taker file

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index')
+const app = express();
+const PORT = 3001;
+
+app.use(express.static('public'));
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/api', api);
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+    });
+    
+    
+    app.get('/notes', (req,res)=>{
+        res.sendFile(path.join(__dirname, 'public/notes.html'));
+    });
+    
+
+require('./routes/routes')(app);
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});  
